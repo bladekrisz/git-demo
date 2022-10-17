4732dcb362e1bdca17e6267526d495d4b2659994
bladekrisz <blade.krisz@gmail.com>
2022-10-17 15:38:33 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,10 +4,3 @@ def main():
     pass
 
 name = input("Give me your name you filthy litl' bitch: ")
-
-def greet(name: str):
-    print(f'Hello {name}!')
-
-greet(name)
-
-# TODO: implement main function
