e437764e133667528eb72df3a7d4d95ae7498447
Bevíz Elek <bevz@elek.com>
2022-10-17 14:52:49 +0200

    Updated demo.py
    Greet by input

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
 # This is a simple hello world program.
 
-name = input("Give me your name you filthy litl bitch: ")
+name = input("Give me your name you filthy litl' bitch: ")
 print(f'Hello {name}!')
